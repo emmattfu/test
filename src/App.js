efa82bab5fb04062854b784855bc424e203e5ec8
emmattfu <makcimdjan@gmail.com>
2020-07-14 21:04:02 +0300

    fetching fix2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from "react";
 
 import { Header, Home, Footer } from "./components";
-import {Route} from 'react-router-dom';
+import {Route} from 'react-router-dom'
+import { Container } from "react-bootstrap";
 
 import "./styles/styles.css";
 
@@ -9,20 +10,23 @@ function App() {
   const [data, setData] = useState('')
 
   useEffect(() => {
-    fetch('https://gist.github.com/alexandrov-va/7f353ca822d074d7ce22d3af3d13696f')
+    fetch('https://gist.githubusercontent.com/alexandrov-va/7f353ca822d074d7ce22d3af3d13696f/raw/133f20af7792d6996f8f941e3aacc86d0cef2f49/page.json')
     .then(resp => resp.json())
-    .then(pageInfo => setData(pageInfo))
+    .then(data => setData(data))
   }, [])
 
   if (!data) {
     return (
       <div className="App">
         <Header />
-        <h1>Loading data...</h1>
+          <Container>
+            <h1>Loading data...</h1>
+          </Container>
         <Footer />
       </div>
     );
   }
+
 
   return (
     <div className="App">
@@ -34,4 +38,3 @@ function App() {
 }
 
 export default App;
-
